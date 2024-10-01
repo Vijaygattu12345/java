3b62ed264db73a2322be39429bdc3e8f1eb380be
Vijaygattu12345 <vijayvittal72@gmail.com>
2024-10-01 12:03:54 +0100

    pushing code

---
+++ TotalPrice.java
@@ -0,0 +1,15 @@
+public class TotalPrice{
+public static void main(String args[]){
+String productName ="iphone";
+double price=75000;
+double gst =0.18;
+double totalPrice = price * gst;
+System.out.println("product name:"+productName);
+double totalgstprice = totalPrice +price;
+System.out.println("total price is:"+totalPrice);
+System.out.println("totalgstprice:"+t_p);
+
+
+
+} 
+}
